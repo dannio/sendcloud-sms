b4a93e0a177034c9f67efd69ff80af92835fbd22
Dan Wong <dan.wong@o2-v2.com>
2017-07-18 16:40:12 +0800

    alteration to sign function to allow multiple functions call this and added get function

--- lib/sendcloud-sms.rb
+++ lib/sendcloud-sms.rb
@@ -16,32 +16,22 @@ module SendCloud
       @api_key = api_key
     end
 
-    def self.sign(template, phone, vars)
+    def self.sign(attritutes)
       param_str = "#{@api_key}&"
-      {
-          smsUser: @user,
-          templateId: template,
-          msgType: 0,
-          phone: (phone.is_a?Array) ? phone.join(',') : phone,
-          vars: vars.to_json
-      }.sort {|a, b| a.to_s <=> b.to_s}.map { |item| param_str << "#{item[0]}=#{item[1]}&" }
-      param_str << @api_key
-      Digest::MD5.new.update(param_str)
-    end
-
-    def self.sign_voice(phone, code)
-      param_str = "#{@api_key}&"
-      {
-          smsUser: @user,
-          phone: phone,
-          code: code
-      }.sort {|a, b| a.to_s <=> b.to_s}.map { |item| param_str << "#{item[0]}=#{item[1]}&" }
+      param_str << attritutes.sort {|a, b| a.to_s <=> b.to_s}.map { |item| param_str << "#{item[0]}=#{item[1]}&" }
       param_str << @api_key
       Digest::MD5.new.update(param_str)
     end
 
     def self.send(template, phone, vars)
-      signature = sign(template, phone, vars)
+      # signature requires template, phone, vars
+      signature = sign({
+          smsUser: @user,
+          templateId: template,
+          msgType: 0,
+          phone: (phone.is_a?Array) ? phone.join(',') : phone,
+          vars: vars.to_json
+      })
       response = RestClient.post 'http://sendcloud.sohu.com/smsapi/send?',
                                  smsUser: @user,
                                  templateId: template,
@@ -53,13 +43,32 @@ module SendCloud
     end
 
     def self.send_voice(phone, code)
-      signature = sign_voice(phone, code)
+      # signature requires phone, code
+      signature = sign({
+          smsUser: @user,
+          phone: phone,
+          code: code
+      })
       response = RestClient.post 'http://sendcloud.sohu.com/smsapi/sendVoice?',
                                  smsUser: @user,
                                  phone: phone,
                                  code: code,
                                  signature: signature
       JSON.parse(response.to_s)['statusCode']
+    end
+
+    def self.get(template)
+      # signature requires template
+      signature = sign({
+          smsUser: @user,
+          templateId: template
+      })
+      response = RestClient.get 'http://www.sendcloud.net/smsapi/get?',
+                                 smsUser: @user,
+                                 templateId: template,
+                                 signature: signature
+      JSON.parse(response.to_s)['statusCode']
+      
     end
   end
 end
